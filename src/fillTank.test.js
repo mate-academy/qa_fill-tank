2da74ed167e29644ca6733bf220b224f4c80850f
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-10-19 13:32:33 +0200

    qa_fill-tank

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -5,7 +5,7 @@ const { fillTank } = require('./fillTank');
 describe('fillTank', () => {
   it('should fill the tank completely', () => {
     const customer = {
-      money: 130,
+      money: 60, // Money for 30 liters at the price of 2 per liter
       vehicle: {
         maxTankCapacity: 40,
         fuelRemains: 5,
@@ -19,16 +19,16 @@ describe('fillTank', () => {
 
   it('should partially fill the tank', () => {
     const customer = {
-      money: 100,
+      money: 50, // Money for 25 liters at the price of 2 per liter
       vehicle: {
         maxTankCapacity: 40,
         fuelRemains: 5,
       },
     };
     const fuelPrice = 2;
-    fillTank(customer, fuelPrice, 20);
-    expect(customer.vehicle.fuelRemains).toBe(25);
-    expect(customer.money).toBe(60);
+    fillTank(customer, fuelPrice, 25);
+    expect(customer.vehicle.fuelRemains).toBe(30);
+    expect(customer.money).toBe(0);
   });
 
   it('should not pour less than 2 liters', () => {
@@ -47,7 +47,7 @@ describe('fillTank', () => {
 
   it('should pour only what the customer can pay', () => {
     const customer = {
-      money: 20,
+      money: 30, // Money for 10 liters at the price of 3 per liter
       vehicle: {
         maxTankCapacity: 40,
         fuelRemains: 5,
@@ -55,7 +55,7 @@ describe('fillTank', () => {
     };
     const fuelPrice = 3;
     fillTank(customer, fuelPrice);
-    expect(customer.vehicle.fuelRemains).toBeCloseTo(10.83, 2);
+    expect(customer.vehicle.fuelRemains).toBe(15); // 10 liters filled
     expect(customer.money).toBe(0);
   });
 });
