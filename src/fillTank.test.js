a137f8bba5e19d65fe4a8478d70563da03b6866a
VladSlob <141657806+VladSlob@users.noreply.github.com>
2023-11-17 07:50:01 +0200

    fillTank.test.js

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -36,22 +36,25 @@ describe('fillTank', () => {
     expect(customer.money).toBe(3000 - (20 * fuelPrice));
   });
 
-  it('should not fill the tank if the customer cannot afford the amount of fuel', () => {
-    const customer = {
-      money: 100,
-      vehicle: {
-        maxTankCapacity: 40,
-        fuelRemains: 8,
-      },
-    };
-    const fuelPrice = 10;
-    const amount = 20;
+  it(
+    'should not fill the tank if the customer cannot afford the amount of fuel',
+    () => {
+      const customer = {
+        money: 100,
+        vehicle: {
+          maxTankCapacity: 40,
+          fuelRemains: 8,
+        },
+      };
+      const fuelPrice = 10;
+      const amount = 20;
 
-    fillTank(customer, fuelPrice, amount);
+      fillTank(customer, fuelPrice, amount);
 
-    expect(customer.vehicle.fuelRemains).toBe(8);
-    expect(customer.money).toBe(100);
-  });
+      expect(customer.vehicle.fuelRemains).toBe(8);
+      expect(customer.money).toBe(100);
+    }
+  );
 
   it('should not fill the tank if the amount is less than 2 liters', () => {
     const customer = {
