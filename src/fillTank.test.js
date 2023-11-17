f64e873d271f78de1baede23e4dc9001fb920dff
VladSlob <141657806+VladSlob@users.noreply.github.com>
2023-11-17 08:07:24 +0200

    fillTank.test.js

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -41,39 +41,39 @@ describe('fillTank', () => {
   });
 
   it(
-    'should not fill the tank if the customer cannot afford the amount of fuel',
-    () => {
-      const customer = {
-        money: 100,
-        vehicle: {
-          maxTankCapacity: 40,
-          fuelRemains: 8,
-        },
-      };
-      const fuelPrice = 10;
-      const amount = 20;
-
-      fillTank(customer, fuelPrice, amount);
-
-      expect(customer.vehicle.fuelRemains).toBe(8);
-      expect(customer.money).toBe(100);
-    }
-  );
-
-  it('should not fill the tank if the amount is less than 2 liters', () => {
+  'should not fill the tank if the customer cannot afford the amount of fuel',
+  () => {
     const customer = {
-      money: 3000,
+      money: 100,
       vehicle: {
         maxTankCapacity: 40,
-        fuelRemains: 38,
+        fuelRemains: 8,
       },
     };
     const fuelPrice = 10;
-    const amount = 2;
+    const amount = 20;
 
     fillTank(customer, fuelPrice, amount);
 
-    expect(customer.vehicle.fuelRemains).toBe(38);
-    expect(customer.money).toBe(3000);
-  });
+    expect(customer.vehicle.fuelRemains).toBe(8);
+    expect(customer.money).toBe(100);
+  }
+);
+
+it('should not fill the tank if the amount is less than 2 liters', () => {
+  const customer = {
+    money: 3000,
+    vehicle: {
+      maxTankCapacity: 40,
+      fuelRemains: 38,
+    },
+  };
+  const fuelPrice = 10;
+  const amount = 1.5;
+
+  fillTank(customer, fuelPrice, amount);
+
+  expect(customer.vehicle.fuelRemains).toBe(38);
+  expect(customer.money).toBe(3000);
+ });
 });
