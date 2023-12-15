3d97d3ef5426d890dee09e3949ec36acbed56afd
Eugene Khablenko <eugene.khablenko@gmail.com>
2023-12-15 03:38:36 +0200

    first commit

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -1,11 +1,41 @@
 'use strict';
 
 describe('fillTank', () => {
-  // const { fillTank } = require('./fillTank');
+  const { fillTank } = require('./fillTank');
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(fillTank).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it("doesn't return anything", () => {
+    const customer = {
+      money: 3000, // customer account balance
+      vehicle: {
+        maxTankCapacity: 40, // fuel tank volume
+        fuelRemains: 8, // Remaining fuel in the tank
+      },
+    };
+
+    expect(fillTank(customer)).toBe(undefined);
+  });
+
+  it('full tank is ordered if amount is not passed', () => {
+    const customer = {
+      money: 3000, // customer account balance
+      vehicle: {
+        maxTankCapacity: 40, // fuel tank volume
+        fuelRemains: 8, // Remaining fuel in the tank
+      },
+    };
+
+    fillTank(customer, 50);
+
+    expect(customer.vehicle.fuelRemains).toBe(40);
+  });
+
+  it('', () => {});
+
+  it('', () => {});
+
+  it('', () => {});
 });
