59c059d299790429d979920f0bd565f34b7a55ed
Yehor Korablin <yegor_ka2004@ukr.net>
2024-01-22 07:13:57 +0200

    Added tests

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -1,11 +1,69 @@
 'use strict';
 
 describe('fillTank', () => {
-  // const { fillTank } = require('./fillTank');
+  const { fillTank } = require('./fillTank');
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(fillTank).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should return nothing', () => {
+    expect(fillTank(
+      {
+        money: 3000,
+        vehicle: {
+          maxTankCapacity: 40,
+          fuelRemains: 8,
+        },
+      },
+      50
+    )).toBe(undefined);
+  });
+
+  it('should change quantity of money and fuel', () => {
+    const tank = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(tank, 50);
+
+    expect(tank).toHaveProperty('money', 1400);
+
+    expect(tank.vehicle)
+      .toHaveProperty('fuelRemains', tank.vehicle.maxTankCapacity);
+  });
+
+  it('should not change quantity of money and fuel', () => {
+    const tank = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(tank, 50, 1);
+
+    expect(tank).toHaveProperty('money', 3000);
+    expect(tank.vehicle).toHaveProperty('fuelRemains', 8);
+  });
+
+  it('should change quantity of money and fuel', () => {
+    const tank = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(tank, 50, 20);
+
+    expect(tank).toHaveProperty('money', 2000);
+    expect(tank.vehicle).toHaveProperty('fuelRemains', 28);
+  });
 });
